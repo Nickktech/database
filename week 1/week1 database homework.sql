0ed9d6b9e91a7400f6d7d6d2cecc637a742e95d6
Nickktech <suciogb@outlook.com>
2026-08-21 15:04:24 -0600

    Add files via upload

---
+++ week 1/week1 database homework.sql
@@ -0,0 +1,11 @@
+select 1+1;
+select current_date;
+select current_database();
+select version();	
+select 'Hello, SQL!';
+
+part B 
+1. create a database by (create database anylsis)
+2. table with basic syntax (create TABLE teachers (id bigserial,firstname,lastname,school,hiredate,salary,); this is a table named teacher with 6 colums)
+3. ; are important because it ends the command its part of a ansi sql standard
+4. 2 examples are name,start date
